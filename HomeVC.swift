17007051d8a78f25f4e162f83b36970179f7454b
Parker <pakalewis@gmail.com>
2014-10-27 15:32:10 -0700

    Set up characters array in HomeVC

--- HomeVC.swift
+++ HomeVC.swift
@@ -12,6 +12,7 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
 
     
     @IBOutlet weak var collectionView: UICollectionView!
+    var characters = [Character]()
 
     
     override func viewDidLoad() {
@@ -22,14 +23,35 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         // register CharacterCell nib for the collection view
         let nib = UINib(nibName: "CharacterCell", bundle: NSBundle.mainBundle())
         self.collectionView.registerNib(nib, forCellWithReuseIdentifier: "CHARACTER_CELL")
+        
+        MarvelNetworking.controller.getCharacters(nameQuery: "Spiasdfadsfasdfder") { (errorString, charactersArray) -> Void in
+            
+            if charactersArray != nil {
+                self.characters = Character.parseJSONIntoCharacters(data: charactersArray!)
+            } else {
+                println("no data")
+            }
+            self.collectionView.reloadData()
+        }
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 50
+        return self.characters.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("CHARACTER_CELL", forIndexPath: indexPath) as CharacterCell
+        
+        let currentCharacter = self.characters[indexPath.row]
+        
+        MarvelNetworking.controller.performRequestWithURLString(currentCharacter.thumbnailURL, method:"GET", parameters: nil, acceptJSONResponse: true, sendBodyAsJSON: false, completion: {(data, errorString) -> Void in
+            
+            cell.imageView.image = UIImage(data: data)
+        
+        })
+        //cell.imageView.image = currentCharacter.
+//        cell.imageView.backgroundColor = UIColor.yellowColor()
+        cell.nameLabel.text = "NAME"
         
         return cell
     }
