71db16f7cde1e9df4c7858848b176c0c4594480e
Parker <pakalewis@gmail.com>
2014-10-27 15:48:36 -0700

    Images download for cells in collection view

--- HomeVC.swift
+++ HomeVC.swift
@@ -24,7 +24,7 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         let nib = UINib(nibName: "CharacterCell", bundle: NSBundle.mainBundle())
         self.collectionView.registerNib(nib, forCellWithReuseIdentifier: "CHARACTER_CELL")
         
-        MarvelNetworking.controller.getCharacters(nameQuery: "Spiasdfadsfasdfder") { (errorString, charactersArray) -> Void in
+        MarvelNetworking.controller.getCharacters(nameQuery: "S") { (errorString, charactersArray) -> Void in
             
             if charactersArray != nil {
                 self.characters = Character.parseJSONIntoCharacters(data: charactersArray!)
@@ -41,18 +41,23 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = self.collectionView.dequeueReusableCellWithReuseIdentifier("CHARACTER_CELL", forIndexPath: indexPath) as CharacterCell
+
+        cell.imageView.image = nil
+        
+        var currentTag = cell.tag + 1
+        cell.tag = currentTag
         
         let currentCharacter = self.characters[indexPath.row]
-        
-        MarvelNetworking.controller.performRequestWithURLString(currentCharacter.thumbnailURL, method:"GET", parameters: nil, acceptJSONResponse: true, sendBodyAsJSON: false, completion: {(data, errorString) -> Void in
-            
-            cell.imageView.image = UIImage(data: data)
-        
-        })
-        //cell.imageView.image = currentCharacter.
-//        cell.imageView.backgroundColor = UIColor.yellowColor()
-        cell.nameLabel.text = "NAME"
-        
+        cell.nameLabel.text = currentCharacter.name
+
+        if currentCharacter.thumbnailURL != nil {
+            MarvelNetworking.controller.getImageAtURLString(currentCharacter.thumbnailURL!, completion: { (image, errorString) -> Void in
+                
+                if cell.tag == currentTag {
+                    cell.imageView.image = image
+                }
+            })
+        }
         return cell
     }
     
