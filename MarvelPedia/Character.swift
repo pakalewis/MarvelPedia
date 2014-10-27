d090f96747892c6d0d9fa290ecd55ce7fa10342d
Parker <pakalewis@gmail.com>
2014-10-27 10:59:45 -0700

    test commit of Character class

---
+++ MarvelPedia/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  MarvelPedia
+//
+//  Created by Parker Lewis on 10/27/14.
+//  Copyright (c) 2014 CodeFellows. All rights reserved.
+//
+
+import Foundation
+
+
+class Character {
+    
+    // test file
+    
+}
