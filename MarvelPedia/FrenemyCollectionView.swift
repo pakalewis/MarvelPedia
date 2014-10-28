e231eea81f566f3916c1cc8cd43192fa0cc58f6f
Parker <pakalewis@gmail.com>
2014-10-28 13:40:26 -0700

    start of collectionview in tableview cell

---
+++ MarvelPedia/FrenemyCollectionView.swift
@@ -0,0 +1,16 @@
+//
+//  FrenemyCollectionView.swift
+//  MarvelPedia
+//
+//  Created by Parker Lewis on 10/28/14.
+//  Copyright (c) 2014 CodeFellows. All rights reserved.
+//
+
+import UIKit
+
+class FrenemyCollectionView: UICollectionView {
+
+    
+    
+
+}
