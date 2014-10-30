ca011dc4ee8d0aee5463abdd419f4e61c9f5074d
coffellas-cto <cto@coffellas.com>
2014-10-30 16:31:37 -0700

    Added UIColor extension

---
+++ MarvelPedia/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  MarvelPedia
+//
+//  Created by Alex G on 30.10.14.
+//  Copyright (c) 2014 CodeFellows. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func colorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
